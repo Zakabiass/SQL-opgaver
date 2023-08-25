bad90d486eba28ed7ea1c6d767668e416a690c13
Zakabiass <141635072+Zakabiass@users.noreply.github.com>
2023-08-25 10:52:52 +0200

    Add files via upload

---
+++ SQL Index.cs
@@ -0,0 +1,20 @@
+﻿namespace SQL_INDEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\ZakariasBigSad\my_file.txt";
+            Random random = new Random();
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    sw.WriteLine(i + ", " + random.Next(1, 10000));
+                }
+            }
+            
+        }
+    }
+}
